4d8554df266d3363ba60aeddac8b2d965971fbd1
Jayant Khandelwal <jayant.khandelwal7@gmail.com>
2020-07-03 16:07:16 +0530

    port updates

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ if(process.env.NODE_ENV=="production"){
 
 var PORT = process.env.PORT || 3000;
 
-app.listen(3000,process.env.IP,()=>{
+app.listen(PORT,process.env.IP,()=>{
 	console.log("Server Has Started");
 })
