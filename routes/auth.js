c9772011e0471c358a081a2973fcf27c23c82d38
Jayant Khandelwal <jayant.khandelwal7@gmail.com>
2020-07-04 19:08:00 +0530

    email added

--- routes/auth.js
+++ routes/auth.js
@@ -10,8 +10,26 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken'); 
 const {JWT_TOKEN} = require('../keys')
 
+const crypto = require('crypto');
+
 const verifyToken = require('../middleware/verifyToken')
 
+//nodemailer
+//const nodemailer = require('nodemailer');
+//const sendgridTransport = require('nodemailer-sendgrid-transport');
+
+// const transporter = nodemailer.createTransport(sendgridTransport({
+// 	auth:{
+// 		api_key: "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"
+// 	}
+// }))
+
+var SENDGRID_API_KEY = "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";
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+//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
 
 
 router.get('/protected',verifyToken,(req,res,next)=>{
@@ -39,7 +57,17 @@ router.post('/signup',(req,res,next)=>{
 					email:email,
 					pic:pic
 				})
-				.then(name=>{
+				.then(user=>{
+
+					//code to send a mail when user is successfully signed up
+					const msg = {
+  					to: user.email,
+  					from: 'chatwowapp@gmail.com',
+				  	subject: 'Welcome to chatwow',
+  					text: 'We are happy to have you onboard with us!',
+  					html: '<strong>Engage and Explore with us!</strong>'
+					};
+					sgMail.send(msg);
 					res.json({message:"Successfully signed up"})
 				})
 				.catch(error=>{
@@ -94,5 +122,38 @@ router.post('/signin',(req,res,next)=>{
 	
 	
 })
+
+
+// router.post('/reset-password',(req,res)=>{
+// 	crypto.randomBytes(36,(err,buffer)=>{
+// 		if(err) console.log(err);
+
+// 						//conveting hexadecimal to string
+// 		const token = buffer.toString("hex");
+// 		User.find({email: req.body.email})
+// 		.then((user)=>{
+// 			if(!user)
+// 				return res.status(422).json({error:"user don't exist with this email"});
+			
+// 			user.resetToken = token;
+// 			user.expireToken = Date.now() + (3600*1000);
+// 			user.save().then((result)=>{
+// 				const msg = {
+//   					to: user.email,
+//   					from: 'chatwowapp@gmail.com',
+// 				  	subject: 'Welcome to chatwow',
+//   					html: '<b>Your Request for Reset password!</b><h5>click on this <a href="http://localhost:3001/reset/${token}">link</a> to change the password</h5>'
+// 					};
+// 					sgMail.send(msg);
+// 					res.json({message:"check your email"})
+// 			})
+// 			.catch(err=>{
+// 				res.json({message:"some error occured"})	
+// 			})
+
+// 		})
+// 	})
+// })
+
 
 module.exports=router
